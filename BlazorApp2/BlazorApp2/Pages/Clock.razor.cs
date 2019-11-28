743f312cafbbe569ae1f079ca88fd40b883a670d
Renzo Robles <rrobles@brainshark.com>
2019-11-27 19:16:09 -0500

    Adding demos content

---
+++ BlazorApp2/BlazorApp2/Pages/Clock.razor.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+using System.Threading.Tasks;
+using System.Timers;
+
+namespace BlazorApp2.Pages
+{
+    public partial class Clock : ComponentBase
+    {
+        [Parameter]
+        public string Message { get; set; }
+
+        [Parameter]
+        public RenderFragment ChildContent { get; set; }
+
+        string currentTime = "N/A";
+        string buttonAction = "N/A";
+        string currentCss = "clock-notset";
+        Timer timer;
+        
+        protected override void OnInitialized()
+        {
+            InitTimer();
+            StartTimer();
+        }
+
+        private void StartStop()
+        {
+            if (timer.Enabled)
+                StopTimer();
+            else
+                StartTimer();
+        }
+
+        private void StartTimer()
+        {
+            buttonAction = "STOP";
+            timer.Start();
+        }
+
+        private void StopTimer()
+        {
+            buttonAction = "START";
+            timer.Stop();
+        }
+
+        private void InitTimer()
+        {
+            timer = new Timer(1000);
+            timer.Elapsed += async (sender, e) => await TimerTick();
+        }
+
+        private Task TimerTick()
+        {
+            currentTime = DateTime.Now.ToLongTimeString();
+            currentCss = "clock-working";
+            this.InvokeAsync(StateHasChanged);
+
+            return Task.CompletedTask;
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # BlazorServerDemos
+
+This repository contains the demos from http://www.renzorobles.com/video-blazor-server-con-net-core-3-0/
